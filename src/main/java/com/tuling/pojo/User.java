5cdc0f234eb77737c5c1e5f99dcfe1a6b20f88bd
niuyh <2459100359@qq.com>
2021-02-23 10:23:02 +0800

    修改user

--- src/main/java/com/tuling/pojo/User.java
+++ src/main/java/com/tuling/pojo/User.java
@@ -6,4 +6,22 @@ package com.tuling.pojo;
  * @time: 2021/2/23 10:19
  */
 public class User {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
